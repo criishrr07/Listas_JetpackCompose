ea9a064109449ca887cdd1f7f502942367c99db5
Cristian Herrera <s22120225@alumnos.itsur.edu.mx>
2025-09-20 20:13:31 -0600

    Se implemento una funcion para que el boton funcionara

--- app/src/main/java/com/example/watercounter/WaterCounter.kt
+++ app/src/main/java/com/example/watercounter/WaterCounter.kt
@@ -1,18 +1,24 @@
 package com.example.watercounter
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        // Changes to count are now tracked by Compose
+        val count: MutableState<Int> = remember { mutableStateOf(0) }
+        Text("You've had ${count.value} glasses.")
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
